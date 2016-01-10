37a39cd981e406adb424f35b192e9954943e208c
BukkitPlaysMC <craftyourbukkit@gmail.com>
2016-01-10 21:16:18 +0100

    Changing Main.php to use TextFormat, instead of color codes

--- src/ServerLoveMCPE/Main.php
+++ src/ServerLoveMCPE/Main.php
@@ -36,13 +36,13 @@ class Main extends PluginBase implements Listener{
                     return false;
                 }
                 if (!($sender instanceof Player)){ 
-                $sender->sendMessage("§5[♥] YOU MUST USE THIS COMMAND IN GAME. SORRY.");
+                $sender->sendMessage(TextFormat::DARK_PURPLE . "[♥] YOU MUST USE THIS COMMAND IN GAME. SORRY.");
                     return true;
                 }
                 
                 $loved = array_shift($args);
                 if($this->nolove->exists(strtolower($loved))){
-                    $sender->sendMessage("§5[♥]Sorry, " . $loved . "§5 is not looking to love anyone right now.");
+                    $sender->sendMessage(TextFormat::DARK_PURPLE . "[♥]Sorry, " . $loved . " is not looking to love anyone right now.");
                     return true;
                 }else{
                     $lovedPlayer = $this->getServer()->getPlayer($loved);
@@ -50,23 +50,23 @@ class Main extends PluginBase implements Listener{
                         if($lovedPlayer == $sender){
                             //This is where the loop for the #ForeverAlone goes to - by ratchetgame98
                             //You can personlise the messages to your liking also
-                            $sender->sendMessage("§5[♥]You can't love yourself :P");
-                            $this->getServer()->broadcastMessage($sender->getName() . "§5[♥] §etried to love themselves :P. §6#ForeverAlone");
+                            $sender->sendMessage(TextFormat::DARK_PURPLE . "[♥]You can't love yourself :P");
+                            $this->getServer()->broadcastMessage($sender->getName() . TextFormat::DARK_PURPLE . "[♥] §etried to love themselves :P." . TextFormat::GOLD . "#ForeverAlone");
                         }else{
-                            $lovedPlayer->sendMessage($sender->getName()."§5 is in love with you!");
+                            $lovedPlayer->sendMessage($sender->getName() . TextFormat::DARK_PURPLE . " is in love with you!");
                             /** $sender->setNameTag($sender->getName() . "- ♥");
                             * WON'T WORK ATM
                             $loved->setNameTag($loved->getName() . "- ♥"); **/
                             if(isset($args[0])){
                                 $lovedPlayer->sendMessage("Reason: " . implode(" ", $args));
                             }
-                            $sender->sendMessage("§5[♥] So you love §a" . $loved . "?§5 Awww, thats nice");
-                            $this->getServer()->broadcastMessage("§a" . $sender->getName() . " §dis in love with §a" . $loved . "§d.");
-                            $this->getServer()->broadcastMessage("§d♥" . $loved . "§d♥" . $sender->getName() . "§d♥");
+                            $sender->sendMessage(TextFormat::DARK_PURPLE . "[♥] So you love " . TextFormat::GREEN . $loved . "?" . TextFormat::DARK_PURPLE . " Awww, thats nice");
+                            $this->getServer()->broadcastMessage(TextFormat::GREEN . $sender->getName() . TextFormat::LIGHT_PURPLE . " is in love with " . TextFormat::GREEN . $loved . TextFormat::LIGHT_PURPLE . "§d.");
+                            $this->getServer()->broadcastMessage(TextFormat::LIGHT_PURPLE . "♥" . $loved . "♥" . $sender->getName() . "♥");
                             return true;
                         }
                     }else{
-                        $sender->sendMessage("§5[♥] §a" . $loved . "§5 is not avalible for love. #shameful. §5 Basically, §a" . $loved . "§5 does not exist, or is not online.");
+                        $sender->sendMessage(TextFormat::DARK_PURPLE . "[♥] " . TextFormat::GREEN . $loved . TextFormat::DARK_PURPLE . " is not avalible for love. #shameful. Basically, " . TextFormat::GREEN . $loved . TextFormat::DARK_PURPLE . " does not exist, or is not online.");
                         return true;
                     }
                 }
@@ -81,24 +81,24 @@ class Main extends PluginBase implements Listener{
                     return false;
                 }
                 if (!($sender instanceof Player)){ 
-                $sender->sendMessage("§5[♥] YOU MUST USE THIS COMMAND IN GAME. SORRY.");
+                $sender->sendMessage(TextFormat::DARK_PURPLE . "[♥] YOU MUST USE THIS COMMAND IN GAME. SORRY.");
                     return true;
                 }
                 $loved = array_shift($args);
                     $lovedPlayer = $this->getServer()->getPlayer($loved);
                     if($lovedPlayer !== null and $lovedPlayer->isOnline()){
-                        $lovedPlayer->sendMessage("§5[♥]§a" . $sender->getName() ."§5has broken up with you!");
+                        $lovedPlayer->sendMessage(TextFormat::DARK_PURPLE . "[♥]" . TextFormat::GREEN . $sender->getName() . TextFormat::DARK_PURPLE . "has broken up with you!");
                         if(isset($args[0])){
                             $lovedPlayer->sendMessage("Reason: " . implode(" ", $args));
                         }
-                        $sender->sendMessage("§5[♥] You have broken up with §a" . $loved . "§5.");
-                        $this->getServer()->broadcastMessage("§a" . $sender->getName() . " §dhas broken up with §a" . $loved . "§d.");
+                        $sender->sendMessage(TextFormat::DARK_PURPLE . "[♥] You have broken up with " . TextFormat::GREEN . $loved . TextFormat::DARK_PURPLE .  ".");
+                        $this->getServer()->broadcastMessage(TextFormat::GREEN . $sender->getName() . TextFormat::LIGHT_PURPLE . " has broken up with " . TextFormat::GREEN . $loved . TextFormat::LIGHT_PURPLE . ".");
                        /** $sender->setNameTag($sender->getName() . "");
                         * WON'T WORK ATM
                         $loved->setNameTag($loved->getName() . ""); **/
                         return true;
                     }else{
-                        $sender->sendMessage($loved . "§5[♥] is not avalible for a breakup. Basically, §a" . $loved . "§5 does not exist, or is not online.");
+                        $sender->sendMessage($loved . TextFormat::DARK_PURPLE . "[♥] is not avalible for a breakup. Basically, " . TextFormat::GREEN . $loved . TextFormat::DARK_PURPLE . " does not exist, or is not online.");
                         return true;
                     }
 /**
@@ -112,16 +112,16 @@ class Main extends PluginBase implements Listener{
                     return false;
                 }
                 if (!($sender instanceof Player)){ 
-                $sender->sendMessage("§5[♥] YOU MUST USE THIS COMMAND IN GAME. SORRY.");
+                $sender->sendMessage(TextFormat::DARK_PURPLE . "[♥] YOU MUST USE THIS COMMAND IN GAME. SORRY.");
                     return true;
                 }
                 if($args[0] == "nolove"){
                     $this->nolove->set(strtolower($sender->getName()));
-                    $sender->sendMessage("§5[♥] You will no longer be loved. §e#ForEverAlone");
+                    $sender->sendMessage(TextFormat::DARK_PURPLE . "[♥] You will no longer be loved." . TextFormat::YELLOW . "#ForEverAlone");
                     return true;
                 }elseif($args[0] == "love"){
                     $this->nolove->remove(strtolower($sender->getName()));
-                    $sender->sendMessage("§5[♥] You will now be loved again! §e#GetInThere");
+                    $sender->sendMessage(TextFormat::DARK_PURPLE . "[♥] You will now be loved again!"  . TextFormat::YELLOW . "#GetInThere");
                     return true;
                 }else{
                     return false;
@@ -133,11 +133,11 @@ class Main extends PluginBase implements Listener{
 **/
             break;
             case "serverlove":
-                $sender->sendMessage("§5[♥][ServerLoveMCPE] Original ServerLove (For MCPC )  Made By ratchetgame98 ");
-                $sender->sendMessage("§d[♥][ServerLoveMCPE] Usage: /love <playerName>");
-                $sender->sendMessage("§d[♥][ServerLoveMCPE] Usage: /breakup <playerName>");
-                $sender->sendMessage("§d[♥][ServerLoveMCPE] Usage: /nolove <nolove / love> ");
-                $sender->sendMessage("§5[♥][ServerLoveMCPE] Happy Loving!");
+                $sender->sendMessage(TextFormat::DARK_PURPLE . "[♥][ServerLoveMCPE] Original ServerLove (For MCPC )  Made By ratchetgame98 ");
+                $sender->sendMessage(TextFormat::LIGHT_PURPLE . "[♥][ServerLoveMCPE] Usage: /love <playerName>");
+                $sender->sendMessage(TextFormat::LIGHT_PURPLE . "[♥][ServerLoveMCPE] Usage: /breakup <playerName>");
+                $sender->sendMessage(TextFormat::LIGHT_PURPLE . "[♥][ServerLoveMCPE] Usage: /nolove <nolove / love> ");
+                $sender->sendMessage(TextFormat::DARK_PURPLE . "[♥][ServerLoveMCPE] Happy Loving!");
                 return true;
             break;
         default:
